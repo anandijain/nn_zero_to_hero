66d2d8b46ef2e6e4ac7aaa7c32b7e63528c311b9
= <=>
2024-11-23 23:32:27 -0800

    negative log likelihood

--- makemore.jl
+++ makemore.jl
@@ -36,8 +36,9 @@ ixs = rand(rng, dist, 100)
 getd(itos, ixs) |> tally
 
 pvecs = map(x -> normalize(x, 1), eachrow(N))
+P = collect(reduce(hcat, pvecs)' )
+@assert allapprox(sum.(pvecs))
 dists = map(Categorical, pvecs)
-
 
 for i in 1:30
     out = []
@@ -52,3 +53,48 @@ for i in 1:30
     end
     println(join(out))
 end
+
+
+#nll
+log_likelihood = 0.
+n = 0
+
+for w in ws 
+    wcs = w_chars(w)
+    for (a, b) in zipit(wcs)
+        ix1, ix2 = getd(stoi, (a, b))
+        prob = P[ix1, ix2]
+        logprob = log(prob)
+        log_likelihood += logprob
+        n +=1 
+    end
+end
+@show log_likelihood
+nll = -log_likelihood
+@show nll
+@show nll/n
+
+
+# train set
+xs, ys = [], []
+
+for w in ws 
+    wcs = w_chars(w)
+    for (a, b) in zipit(wcs)
+        ix1, ix2 = getd(stoi, (a, b))
+        push!(xs, ix1)
+        push!(ys, ix2)
+    end
+end
+
+function onehot(vec, n)  
+    ys = []
+    for x in vec 
+        y = zeros(n)
+        y[x] = 1
+        push!(ys, y)
+    end
+    ys 
+end
+    
+heatmap(stack(onehot(xs[1:5], 27))')
