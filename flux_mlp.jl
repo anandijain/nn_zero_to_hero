103b213f35643b2033a1ea5ac54dcc6ed4abff2d
anand jain <anandj@uchicago.edu>
2024-12-05 13:16:15 -0800

    lesson

--- flux_mlp.jl
+++ flux_mlp.jl
@@ -1,4 +1,4 @@
-using Flux, OneHotArrays, ProgressMeter, StatsBase
+using Flux, OneHotArrays, ProgressMeter, StatsBase, Plots
 using Flux.Losses
 
 function get_dataset(block_size)
@@ -35,6 +35,7 @@ itos = Dict(reverse.(collect(stoi)))
 
 X, Y = get_dataset(block_size)
 nx = size(X, 1)
+Y_oh = onehotbatch(Y, 1:27)
 
 lr = 1
 emb_dim = 2
@@ -54,12 +55,6 @@ mlp = Chain(
     Dense(h_dim, vocab_size)
 )
 
-model = Chain(
-    Embedding(vocab_size, emb_dim),
-    x -> reshape(permutedims(x, (2, 1, 3)), (:, block_size * emb_dim))',
-    Dense(block_size * emb_dim, h_dim, tanh),
-    Dense(h_dim, vocab_size)
-)
 
 C([1, 1, 1]) # get the embedding for a single example returns a 2x3 
 C(X)
@@ -69,34 +64,68 @@ emb = reshape(permutedims(C(X), (2, 1, 3)), (:, block_size * emb_dim))'
 logits = mlp(emb)
 loss = logitcrossentropy(logits, onehotbatch(Y, 1:27))
 
+
+# (xbatch, ybatch) = first(loader)
+# xin = stack(xbatch; dims=1)
+# logits = model(xin)
+# counts = exp.(logits[:, 1] .- maximum(logits[:, 1]))
+# sample(Weights(counts))
+# loss = logitcrossentropy(logits, onehotbatch(ybatch, 1:27))
+# loss, grads = Flux.withgradient(model) do m
+#     logits = m(stack(xbatch; dims=1))
+#     loss = logitcrossentropy(logits, onehotbatch(ybatch, 1:27))
+# end
+# Flux.update!(opt_state, model, grads[1])
+model = Chain(
+    Embedding(vocab_size, emb_dim),
+    x -> reshape(permutedims(x, (2, 1, 3)), (:, block_size * emb_dim))',
+    Dense(block_size * emb_dim, h_dim, tanh;init=randn32),
+    Dense(h_dim, vocab_size; init=randn32)
+)
+
+function norm_rows(A;dims=2)
+    A ./ sum(A; dims)
+end
+
+# the loss with reinitialized weights is 3.38 which is super low, should be like 20
+# GOOD LESSON: 
+# the defalt initialization of weights is glorot_uniform 
+# the low initial loss comes back if we change to standard randn32
+logits = model(X)
+loss = logitcrossentropy(logits, Y_oh)
+counts = exp.(logits .- maximum(logits))
+probabilities = norm_rows(counts')
+loss = -mean(log.(probabilities[CartesianIndex.(1:nx, Y)]))
+
+
+n_samples = 2000 
+lre = LinRange(-4, 0, n_samples)
+lrs = 10 .^ lre
 loader = Flux.DataLoader((eachrow(X), Y), batchsize=64, shuffle=true);
 losses = []
-opt_state = Flux.setup(Flux.Descent(0.01), model)  # will store optimiser momentum, etc.
 
-(xbatch, ybatch) = first(loader)
-xin = stack(xbatch; dims=1)
-logits = model(xin)
-counts = exp.(logits[:, 1] .- maximum(logits[:, 1]))
-sample(Weights(counts))
-loss = logitcrossentropy(logits, onehotbatch(ybatch, 1:27))
-loss, grads = Flux.withgradient(model) do m
-    logits = m(stack(xbatch; dims=1))
-    loss = logitcrossentropy(logits, onehotbatch(ybatch, 1:27))
-end
-Flux.update!(opt_state, model, grads[1])
-
-@showprogress for epoch in 1:1
-    for (xbatch, ybatch) in loader
-        loss, grads = Flux.withgradient(model) do m
-            logits = m(stack(xbatch; dims=1))
-            loss = logitcrossentropy(logits, onehotbatch(ybatch, 1:27))
-        end
-        @show loss
-        Flux.update!(opt_state, model, grads[1])
-        push!(losses, loss)  # logging, outside gradient context
+for (i, (xbatch, ybatch)) in enumerate(loader)
+    loss, grads = Flux.withgradient(model) do m
+        logits = m(stack(xbatch; dims=1))
+        loss = logitcrossentropy(logits, onehotbatch(ybatch, 1:27))
+    end
+    # @show loss
+    Flux.update!(opt_state, model, grads[1])
+    push!(losses, loss)  # logging, outside gradient context
+    lr = lrs[i]
+    opt_state = Flux.setup(Flux.Descent(lr), model)  # will store optimiser momentum, etc.
+    if i == n_samples
+        break
     end
 end
 
+plot(lrs, losses)
+# end
+
+loss = logitcrossentropy(model(X), onehotbatch(Y, 1:27))
+
+
+# GENERATION
 for i in 1:5
     xenc = ones(Int, block_size)
     out = []
@@ -114,3 +143,4 @@ for i in 1:5
     println(join(out))
 
 end
+
